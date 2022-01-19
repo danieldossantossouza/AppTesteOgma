99ea61cfa384bd71217dcd72300e0c202afb796d
Samuel Dos Santos Souza <samuel.souza-terceiro@cea.com.br>
2022-01-19 19:15:35 -0300

    Incluindo checkbox.

--- src/paginas/Perguntas.js
+++ src/paginas/Perguntas.js
@@ -142,7 +142,7 @@ return (
   <br/>
 
 <Link to="/Respostas">
-    <button>voltar </button>
+    <button className="btn btn-primary">Outra Pagina </button>
   </Link>
 </tbody>
 
@@ -158,6 +158,42 @@ return (
           Inclua a pergunta !
         </div>
       </div>
+      <div className='row' style={{margin: "4%"}}>
+      <select class="form-select" aria-label="Default select example">
+         <option selected>Escolha o Tipo de Fromulario</option>
+         <option value="1" >Texto</option>
+         <option value="2">Multipla Escolha</option>
+         <option value="3">Radio Button</option>
+      </select>
+      </div>
+      
+      
+      <div className='row'style={{margin: "4%"}}>
+          Escreva e Marque a Resposta Certa 
+      
+
+      <div className="row" style={{margin: "4%"}}>
+        <input className='top-4 col-md-1' style={{marginTop: "4%"}} type="checkbox"/>
+        <input type="text" class="form-control col-md-11" id="validationDefault05" required/> 
+      </div>
+      <div className="row"style={{margin: "4%"}}>
+        <input className='top-4 col-md-1' style={{marginTop: "4%"}} type="checkbox"/>
+        <input type="text" class="form-control col-md-11" id="validationDefault05" required/> 
+      </div>
+      <div className="row"style={{margin: "4%"}}>
+        <input className='top-4 col-md-1' style={{marginTop: "4%"}} type="checkbox"/>
+        <input type="text" class="form-control col-md-11" id="validationDefault05" required/> 
+      </div>
+      <div className="row"style={{margin: "4%"}}>
+        <input className='top-4 col-md-1' style={{marginTop: "4%"}} type="checkbox"/>
+        <input type="text" class="form-control col-md-11" id="validationDefault05" required/> 
+      </div>
+     
+      </div>
+      
+     
+      
+
     </div>
   </ModalBody>
   <ModalFooter>
