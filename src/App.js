bbd79421ec828e087536e1488f6f2557d2a3af35
Samuel Dos Santos Souza <samuel.souza-terceiro@cea.com.br>
2022-01-31 19:32:16 -0300

    Lista de Pergunta useEffect consumindo API.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import {BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import Perguntas from './paginas/Perguntas';
-import Respostas from './paginas/Respostas';
+import ListaPergunta from './paginas/ListaPergunta';
 import Navbar from './Component/Navbar';
-
-
-
-
 
  function App() {
 
   return (
     <>
-    
+   
   <Router>
     <Navbar/>
     <Routes>
       <Route  path="/" exact element={ <Perguntas />} />
-      <Route  path="/Respostas" element={ <Respostas />}/>     
+      <Route  path="/ListaPergunta" element={ <ListaPergunta />}/>     
     </Routes>          
   </Router>
   </>
